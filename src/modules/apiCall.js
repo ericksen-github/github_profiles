41b1d600fb5304ca1e1e05d6516642a39f45c1c4
James <ericksen.github@gmail.com>
2020-09-16 17:26:21 +0100

    Call works to grab profile api info based on user name.

--- src/modules/apiCall.js
+++ src/modules/apiCall.js
@@ -2,15 +2,21 @@ const APIURL = "https://api.github.com/users/";
 
 const apiFunctions = (() => {
   // starts the api call process
-  async function newCall(user) {
+  async function newCall() {
     try {
-      const response = await fetch(APIURL + user, { mode: "cors" });
+      const response = await fetch(APIURL + "ericksen-github", {
+        mode: "cors",
+      });
 
-      console.log(await response.json());
+      sortUserData(await response.json());
     } catch (error) {
       alert("There was an error with your search.");
     }
   }
+
+  const sortUserData = (data) => {
+    console.log(data.avatar_url);
+  };
 
   return {
     newCall,
