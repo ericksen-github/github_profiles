27fd6efc2b9d9b86330417053878f8ba71032cca
James <ericksen.github@gmail.com>
2020-09-16 18:30:37 +0100

    Adjusted card creation to add new info to display. Working with css to find display look I want.

--- src/modules/createCard.js
+++ src/modules/createCard.js
@@ -1,12 +1,18 @@
 const createCard = (user) => {
   const cardHTML = `
-
     <div class = "card">
+        <img src="${user.avatar_url}" alt = "${user.name}"
+        <div> 
+            <h2>${user.name}</h2>
+            <p>${user.bio}</p>
+        </div>
         <div>
-            <img src="${user.avatar_url}" alt = "${user.name}"
+            <p>Repos</p>
+            <p>${user.public_repos}</p>
         </div>
     </div>`;
 
+  console.log(user);
   document.getElementById("main").innerHTML = cardHTML;
 };
 
