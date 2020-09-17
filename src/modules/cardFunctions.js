be5d9db96a34fd641875e6c18a4db10a885fb547
James <ericksen.github@gmail.com>
2020-09-17 11:46:17 +0100

    Pulling repos into card w/ links now working.

--- src/modules/cardFunctions.js
+++ src/modules/cardFunctions.js
@@ -1,3 +1,5 @@
+const APIURL = "https://api.github.com/users/";
+
 const cardFunctions = (() => {
   const createCard = (user) => {
     const cardHTML = `
@@ -8,15 +10,37 @@ const cardFunctions = (() => {
           <div id = "infoContainer">
             <h2>${user.name}</h2>
             <p id = "bio">${user.bio}</p>
+
             <ul>
               <li>${user.public_repos}<p>Repos</p></li>
               <li>${user.followers}<p>Followers</p></li>
               <li>${user.following}<p>Following</p></li>
             </ul>
+            
+            <div id = "repoContainer"></div>
           </div>
       </div>`;
 
     document.getElementById("main").innerHTML = cardHTML;
+    getRepos(user.login);
+  };
+
+  async function getRepos(userName) {
+    const response = await fetch(APIURL + userName + "/repos");
+    addReposToCard(await response.json());
+  }
+
+  const addReposToCard = (repos) => {
+    const repoContainer = document.getElementById("repoContainer");
+
+    repos.forEach((ele) => {
+      const newLink = document.createElement("a");
+      newLink.classList.add("repoLink");
+      newLink.innerHTML = ele.name;
+      newLink.href = ele.html_url;
+      newLink.target = "_blank";
+      repoContainer.appendChild(newLink);
+    });
   };
 
   return {
